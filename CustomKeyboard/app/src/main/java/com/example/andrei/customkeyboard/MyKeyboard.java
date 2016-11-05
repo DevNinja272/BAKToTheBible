6d4ae7891f992c5b4c9d70ab316640d85ace68d4
addjuarez <ajuarez@zenoss.com>
2016-11-05 17:49:13 -0500

    no message

--- CustomKeyboard/app/src/main/java/com/example/andrei/customkeyboard/MyKeyboard.java
+++ CustomKeyboard/app/src/main/java/com/example/andrei/customkeyboard/MyKeyboard.java
@@ -71,9 +71,9 @@ public class MyKeyboard extends InputMethodService
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
-            case -222: //-222 is just a random ID number I assigned my key on the softkeyboard.
+            case -100: //-222 is just a random ID number I assigned my key on the softkeyboard.
                 PopupMenu popup = new PopupMenu(MyKeyboard.this, kv);
-                popup.getMenuInflater().inflate(R.menu.menu_main, popup.getMenu());
+                popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
                 //menu_main being the xml file for the popup menu
                 popup.show();
                 break;
